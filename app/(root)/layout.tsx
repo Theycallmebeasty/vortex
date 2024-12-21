2606971060f886beb2ffb79dba48ae38173d8449
avvass <amagaji1998@gmail.com>
2024-12-22 00:38:30 +0100

    my changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,33 @@
+import Sidebar  from "@/components/Sidebar";
+import Image from "next/image";
+// import { redirect } from "next/navigation";
+
+import MobileNav from "@/components/MobileNav";
+// import { getLoggedInUser } from "@/lib/actions/user.actions";
+
 export default function RootLayout({
    children,
  }: Readonly<{
    children: React.ReactNode;
  }>) {
+
+
+
+  const loggedIn = { firstName: ' BSTY', lastName:'WorldWide'};
    return (
-      <main>
-         SIDEBAR
-         {children}
+      <main className="flex h-screen w-full font-inter">
+         <Sidebar user={loggedIn} />
+         //  <div className="flex size-full flex-col">
+                <div className="root-layout">
+                 <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+                  <div>
+//                    <MobileNav user={loggedIn} />
+//                </div>
+                </div>
+                {children}
+             </div>
       </main>
    );
  }
+
+
